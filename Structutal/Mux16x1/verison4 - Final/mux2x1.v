e0d5033d50c8e603481bfb65c374fb0a2a8053ce
TheOptimistt <75641873+TheOptimisTT@users.noreply.github.com>
2026-02-25 22:41:37 +0200

    Add Vivado tutorial and update mux2x1
    Add Vivado_Tutorial/Makefile and README to provide a simple xsim workflow (compile, elaborate, sim, run, clean) and quick usage notes. Update Structutal/Mux16x1/verison4 - Final/mux2x1.v to fix missing newline at EOF and append a commented example mux8x4 module built from mux2x1 instances for reference.

--- Structutal/Mux16x1/verison4 - Final/mux2x1.v
+++ Structutal/Mux16x1/verison4 - Final/mux2x1.v
@@ -10,3 +10,20 @@ module mux2x1(in,sel,out);
     and G3 (t3, in[1], sel);
     or  G4 (out, t2, t3);
 endmodule
+
+
+/*
+module mux8x4(in,sel,out);
+    input   [3:0] A;
+    input   [3:0] B;
+    input         sel;
+    output  [3:0] out;
+
+	mux2x1 m1 (.in({A[0],B[0]}),.sel(sel),.out(out[0]));
+	mux2x1 m2 (.in({A[1],B[1]}),.sel(sel),.out(out[1]));
+	mux2x1 m3 (.in({A[2],B[2]}),.sel(sel),.out(out[2]));
+	mux2x1 m4 (.in({A[3],B[3]}),.sel(sel),.out(out[3]));
+endmodule
+
+*/
+
